3a6e7c059d61e506a6ea018773c9d7c27dcb2973
Pablo <pablogp01@gmail.com>
2023-11-11 14:46:29 +0000

    first commit kata 4

---
+++ src/main/java/solftware/ulgpc/Track.java
@@ -0,0 +1,4 @@
+package solftware.ulgpc;
+
+public record Track (String name, String composer, String album, String artist, int duration) {
+}
---
+++ src/main/java/solftware/ulgpc/TrackLoader.java
@@ -0,0 +1,7 @@
+package solftware.ulgpc;
+
+import java.util.List;
+
+public interface TrackLoader {
+    List<Track> loadAll();
+}
